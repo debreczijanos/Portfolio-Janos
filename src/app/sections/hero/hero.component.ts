3e6d3b1ebd03c5f9e55be7b04120cf86821d5fd2
János Debreczi <debreczijanos@yahoo.com>
2025-06-13 23:45:59 +0200

    feat(hero): complete hero section layout (desktop only)

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  imports: [TranslateModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
-export class HeroComponent {
-  constructor(translate: TranslateService) {
+export class HeroComponent implements OnInit {
+  title1Chars: string[] = [];
+  title2Chars: string[] = [];
+
+  constructor(private translate: TranslateService) {
     translate.setDefaultLang('de');
     translate.use('de');
   }
 
+  ngOnInit(): void {
+    this.translate.get('HERO_TITLE').subscribe((text: string) => {
+      this.title1Chars = text.split('');
+    });
+    this.translate.get('HERO_TITLE2').subscribe((text: string) => {
+      this.title2Chars = text.split('');
+    });
+  }
 }
