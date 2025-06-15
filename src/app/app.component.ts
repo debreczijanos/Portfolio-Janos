9d00c70991072a896e8fc60cc4eeadddc9776e22
János Debreczi <debreczijanos@yahoo.com>
2025-06-15 21:10:32 +0200

    feat(aboutME): complete aboutMe section layout (desktop only)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './shared/header/header.component';
 import { HeroComponent } from './sections/hero/hero.component';
-
+import { AboutMeComponent } from './sections/about-me/about-me.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, HeaderComponent, HeroComponent],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HeaderComponent,
+    HeroComponent,
+    AboutMeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
